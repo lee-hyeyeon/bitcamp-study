ec2ea22d8f6153d6821cceeb567c3d6a3d51c29b
lee-hyeyeon <lhy1415@knou.ac.kr>
2021-07-06 18:44:56 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex03/Exam0340.java
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0340.java
@@ -7,28 +7,32 @@ package com.eomcs.lang.ex03;
 //  대신 '유효자릿수'라는 방식으로 대략적으로 값의 범위를 표현한다. 
 //
 public class Exam0340 {
+
+  static float test = 12.375f; //f는 4byte 크기(32bit)의 부동소수점 리터럴
+  static double test2 = 12.375f;
+
   public static void main(String[] args) {
-    
+
     //## 4byte(float) 부동소수점의 유효자릿수
     //소수점을 뺀 후 7자리 숫자까지는 거의 정상적으로 저장된다.
     System.out.println(999.9999f);
     System.out.println(999999.9f);
     System.out.println(9.999999f);
     System.out.println("----------------------------");
-    
+
     //유효자릿수가 7자리를 넘어가는 경우 값이 잘려서 저장될 수 있다.
     System.out.println(987654321.1234567f);
     System.out.println(9.876543211234567f);
     System.out.println(987654321123456.7f);
     System.out.println("----------------------------");
-    
+
     //## 8byte(double) 부동소수점의 유효자릿수
     //소수점을 뺀 후 16자리 숫자까지는 거의 정상적으로 저장된다.
     System.out.println(987654321.1234567);
     System.out.println(9.876543211234567);
     System.out.println(987654321123456.7);
     System.out.println("----------------------------");
-    
+
     //유효자릿수가 16자리를 넘어가는 경우 값이 잘려서 저장될 수 있다.
     System.out.println(987654321.12345678);
     System.out.println(9.8765432112345678);
@@ -44,6 +48,8 @@ public class Exam0340 {
     //  - IEEE-754의 방법에 내재되어 있는 문제다.
     //- 해결책
     //  - 시스템에서 필요한 만큼 소수점 이하 자리수를 적절히 짤라 사용하라!
+    // 부동소숫점 보정작업 필요!
+   
   }
 }
 
